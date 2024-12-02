6ab7c7268e27a8a3c88a7559b366f4216ed84723
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-02 13:36:46 +0800

    add nexamerchant config

--- models/nexamerchant_model.py
+++ models/nexamerchant_model.py
@@ -6,3 +6,6 @@ class NexaMerchant(models.Model):
 
     name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
+    api_url = fields.Char(string='API URL', required=True)
+    api_key = fields.Char(string='API Key', required=True)
+    api_secret = fields.Char(string='API Secret', required=True)
