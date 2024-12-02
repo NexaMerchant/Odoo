be9393bc4fc81c8c86f50312c66983b75bf32b92
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-02 14:12:17 +0800

    fix update

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,6 @@
     'category': 'Website',
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv',
         'views/nexamerchant_view.xml',
         'data/nexamerchant_data.xml',
     ],
