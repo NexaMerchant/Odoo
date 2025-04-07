24f15361918603db76037103e6480753531e9e38
rickon <chenbiao@heomai.com>
2025-04-07 19:36:47 +0800

    创建订单功能完善
    1.新增图片同步逻辑
    2.新增支付同步逻辑
    3.调整spu重复性校验逻辑

--- controllers/order.py
+++ controllers/order.py
@@ -9,6 +9,10 @@ import datetime
 from odoo.tools import config
 import traceback
 import sys
+import requests
+import base64
+import redis
+from time import sleep
 
 _logger = logging.getLogger(__name__)
 
@@ -27,6 +31,12 @@ class OrderController(http.Controller):
             'order_code': '',
             'order_id': 0
         }
+
+        redis_host = config['redis_host']
+        redis_port = config['redis_port']
+        redis_db = config['redis_db']
+        redis_password = config['redis_password']
+        redis_obj = redis.Redis(host=redis_host, port=redis_port, db=redis_db, password=redis_password)
 
         try:
             # 获取请求数据
@@ -58,43 +68,58 @@ class OrderController(http.Controller):
                 ('origin', '=', order['order_number']),
             ], limit=1)
 
+            is_add = False
             if order_info:
                 order_id = order_info.id
             else:
                 # 新增
-                order_id = self._create_order(data, customer_id, currency_id)
+                order_new_obj = self._create_order(data, customer_id, currency_id)
+                order_id = order_new_obj.id
                 if not order_id:
                     return {
                     'success': False,
                     'message': '订单创建失败',
                     'status': 401
                 }
+                is_add = True
 
             # 处理订单详情
             for item in order['line_items']:
-                sku = item.get('sku', {})
-                is_delivery = True if int(sku.get('product_id')) == 1925 else False # 是否是运费单
-                if is_delivery:
-                    # 运费sku 添加运费订单行
-                    self._add_shipping_cost(order_id, item.get('price'))
-                else:
-                    variant_id = self._create_product_attributes(item) # 创建商品属性并返回变体值
-                    if variant_id:
-                        # 计算折扣值 保留两位小数
-                        # price = float(item['price'])  # 先转float
-                        # discount = round((float(item['discount_amount']) / price) * 100, 2)  # 计算百分比并保留2位小数
-                        request.env['sale.order.line'].sudo().search([
-                            ('order_id', '=', order_id),
-                            ('product_id', '=', variant_id)
-                        ], limit=1) or request.env['sale.order.line'].sudo().create({
-                            'order_id': order_id,
-                            'product_id': variant_id,
-                            'product_uom_qty': item.get('qty_ordered'),
-                            'price_unit': float(item['price']) - float(item['discount_amount']),
-                            'is_delivery': False,
-                            'currency_id': currency_id,
-                            # 'discount': discount
-                        })
+                variant_id = self._create_product_attributes(item, redis_obj) # 创建商品属性并返回变体值
+                if variant_id:
+                    # 计算折扣值 保留两位小数
+                    # price = float(item['price'])  # 先转float
+                    # discount = round((float(item['discount_amount']) / price) * 100, 2)  # 计算百分比并保留2位小数
+                    request.env['sale.order.line'].sudo().search([
+                        ('order_id', '=', order_id),
+                        ('product_id', '=', variant_id)
+                    ], limit=1) or request.env['sale.order.line'].sudo().create({
+                        'order_id': order_id,
+                        'product_id': variant_id,
+                        'product_uom_qty': item.get('qty_ordered'),
+                        'price_unit': float(item['price']) - float(item['discount_amount']),
+                        'is_delivery': False,
+                        'currency_id': currency_id,
+                        # 'discount': discount
+                    })
+
+            if is_add:
+                payment_info = order.get('payment')
+                method_str = payment_info.get('method')
+                # return payment_info
+                invoice = order_new_obj._create_invoices()
+                invoice.action_post()
+                journal_id = self._get_journal_id('bank')
+                payment_method_id = self._get_payment_method_id('inbound', method_str)
+                payment = request.env['account.payment'].sudo().create({
+                    'payment_type': 'inbound',  # 收款为 inbound, 付款为 outbound
+                    'partner_type': 'customer',  # 客户为 customer, 供应商为 supplier
+                    'partner_id': int(customer_id),
+                    'amount': float(order['grand_total']),
+                    'payment_method_id': payment_method_id,
+                    'journal_id': journal_id,  # 比如现金、银行账户的 journal
+                })
+                payment.action_post()
 
             # 构建成功响应
             response.update({
@@ -165,7 +190,7 @@ class OrderController(http.Controller):
         """
         pass
 
-    def _create_product_attributes(self, item):
+    def _create_product_attributes(self, item, redis_obj):
         """
         创建商品属性 并返回变体值
         1.商品属性: product.attribute
@@ -178,23 +203,28 @@ class OrderController(http.Controller):
             attributes = sku.get('attributes', {})
 
             # 查找或创建spu
-            product_template = request.env['product.template'].sudo().search([
-                ('name', '=', item.get('name')), # 这里需进一步优化
-            ], limit=1)
+            default_code = item.get('default_code').lower()
+            redis_key = 'odoo_product_template_id_map'
+            redis_field = f'{default_code}'
+            product_template_id = redis_obj.hget(redis_key, redis_field)
+            if not product_template_id:
+                product_template = request.env['product.template'].sudo().search([
+                    ('default_code', '=', default_code),
+                ], limit=1)
+                if not product_template:
+                    product_template = request.env['product.template'].sudo().create({
+                        'name': item.get('name', ''),
+                        'description': sku.get('description', ''),
+                        'list_price': float(item.get('price', 0)),
+                        'type': 'consu',
+                    })
+                    product_template_id = product_template.id
+                else:
+                    product_template_id = product_template.id
 
-            if not product_template:
-                product_template = request.env['product.template'].sudo().create({
-                    'name': item.get('name', ''),
-                    'description': sku.get('description', ''),
-                    'list_price': float(item.get('price', 0)),
-                    'type': 'consu',
-                    # 'compare_list_price': item.get('price', 0),
-                    # 'default_code': product_id,
-                    # 'barcode': product_id,
-                    # 'website_id': int(config['usa_website_id']),
-                    # "responsible_id": 17,
-                    # "is_storable": True,
-                })
+                redis_obj.hset(redis_key, redis_field, int(product_template_id))
+
+            product_template_id = int(product_template_id)
 
             # 批量处理属性
             attribute_value_ids = []
@@ -226,7 +256,7 @@ class OrderController(http.Controller):
 
                 # 3. 处理属性线
                 product_attribute_line = request.env['product.template.attribute.line'].sudo().search([
-                    ('product_tmpl_id', '=', product_template.id),
+                    ('product_tmpl_id', '=', product_template_id),
                     ('attribute_id', '=', product_attribute.id),
                 ], limit=1)
 
@@ -236,39 +266,80 @@ class OrderController(http.Controller):
                         product_attribute_line.write({'value_ids': [(4, attribute_value.id)]})
                 else:
                     request.env['product.template.attribute.line'].sudo().create({
-                        'product_tmpl_id': product_template.id,
+                        'product_tmpl_id': product_template_id,
                         'attribute_id': product_attribute.id,
                         'value_ids': [(6, 0, [attribute_value.id])],  # 6,0 确保唯一
                     })
 
-            variants = product_template.product_variant_ids
-            if not variants:
-                product_template._create_variant_ids()
-
             # 4. 查找匹配的变体
-            variant = request.env['product.product'].sudo().search([
-                ('product_tmpl_id', '=', product_template.id),
-                ('product_template_attribute_value_ids.product_attribute_value_id', 'in', attribute_value_ids),
-            ], limit=1)
+            domain = [('product_tmpl_id', '=', product_template_id)]
+            if attribute_value_ids:
+                domain.append(('product_template_attribute_value_ids.product_attribute_value_id', 'in', attribute_value_ids))
+            variant = request.env['product.product'].sudo().search(domain, limit=1)
 
-            if variant:
-                variant.write({'default_code': sku.get('product_sku')})
-                # 待补充sku图片 ToDo
-                return variant.id
-            else:
-                _logger.error(f"变体未找到 模板ID: {product_template.id}, 属性值IDs: {attribute_value_ids}")
-                raise ValueError(f"变体未找到 模板ID: {product_template.id}, 属性值IDs: {attribute_value_ids}")
+            if not variant:
+                product_template._create_variant_ids()
+                variant = request.env['product.product'].sudo().search(domain, limit=1)
+
+                if not variant:
+                    variant = request.env['product.product'].sudo().create({
+                        'product_tmpl_id': product_template_id,
+                        'attribute_value_ids': [(6, 0, attribute_value_ids)],
+                        'default_code': sku.get('product_sku')
+                    })
+
+            # 5. 更新变体信息
+            update_vals = {
+                'default_code': sku.get('product_sku'),
+            }
+            if sku.get('img'):
+                image_base64 = self._get_product_img(variant.id, sku.get('img'))
+                if image_base64:
+                    update_vals['image_1920'] = image_base64
+
+            variant.sudo().write(update_vals)
+
+            return variant.id
 
         except Exception as e:
-            _logger.error("Failed to create product attributes: %s", str(e))
-            raise ValueError("Failed to create product attributes: %s", str(e))
+            raise ValueError(f"Failed to create product attributes---: { str(e)}")
+
+    def _get_product_img(self, variant_id, image_src):
+        # 创建目录
+        os.makedirs('images', exist_ok=True)
+
+        image_path = f'images/{variant_id}.jpg'
+
+        # 下载图片
+        if not os.path.exists(image_path):
+            try:
+                image = requests.get(image_src)
+                image.raise_for_status()
+
+                with open(image_path, 'wb') as file:
+                    file.write(image.content)
+            except Exception as e:
+                print(f"Error downloading image: {e}")
+                raise ValueError("Error downloading image1: %s", str(e))
+
+        # 保存到本地
+        try:
+            with open(image_path, 'rb') as file:
+                return base64.b64encode(file.read()).decode('utf-8')
+        except Exception as e:
+            print(f"Error reading image file: {e}")
+            raise ValueError("Error downloading image2: %s", str(e))
+
+    def _format_created_at(self, created_at):
+        parsed_date = datetime.datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%fZ')
+        formatted_date = parsed_date.strftime('%Y-%m-%d %H:%M:%S')
+        return formatted_date
 
     def _create_order(self, data, customer_id, currency_id):
         try:
             order = data.get('order')
 
-            parsed_date = datetime.datetime.strptime(order['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
-            formatted_date = parsed_date.strftime('%Y-%m-%d %H:%M:%S')
+            formatted_date = self._format_created_at(order['created_at'])
 
             order_data = {
                 'partner_id'    : int(customer_id),
@@ -280,10 +351,6 @@ class OrderController(http.Controller):
                 'currency_id'   : currency_id,
                 'amount_total'  : float(order['grand_total']),
                 'amount_tax'    : float(order['tax_amount']),
-                # 'name'          : config['usa_order_prefix'].replace('\"', '') + str(order['order_number']),
-                # 'warehouse_id'  : 2,
-                # 'company_id'    : 1,
-                # 'website_id'    : int(config['usa_website_id']),
             }
             # return order_data
             order = request.env['sale.order'].sudo().create(order_data)
@@ -293,7 +360,42 @@ class OrderController(http.Controller):
 
         # print(order_data)
 
-        return order.id
+        return order
+
+    def _get_payment_method_id(self, payment_type='inbound', payment_name='paypal_smart_button'):
+        """
+        获取付款方式的ID，根据支付名称映射匹配 account.payment.method
+        """
+        payment_mapping = {
+            'paypal_smart_button': 'paypal',
+            'airwallex': 'paypal'
+        }
+        name = payment_mapping.get(payment_name)
+        payment_method = request.env['account.payment.method'].sudo().search([
+            ('name', 'ilike', name),
+            ('payment_type', '=', payment_type)
+        ], limit=1)
+
+        if not payment_method:
+            raise ValueError(f"[PaymentMethod] Not found: {name} ({payment_type})")
+
+        return payment_method.id
+
+
+    def _get_journal_id(self, type='bank'):
+        """
+        获取支付方式对应的账户（Journal）的ID
+        """
+
+        journal = request.env['account.journal'].sudo().search([
+            ('type', '=', type),
+        ], limit=1)
+
+        if not journal:
+            raise ValueError(f"[Journal] Not found: ({type})")
+
+        return journal.id
+
 
     def _get_currency_id(self, data):
         order = data.get('order')
